948876a087ba37da103be18c0a4de82f0e7e8432
arsitha20 <158494218+arsitha20@users.noreply.github.com>
2024-11-21 19:24:48 -0500

    added comments

--- src/main/java/_5/hw/assignment3/surveyForm/repository/studentSurveyRepository.java
+++ src/main/java/_5/hw/assignment3/surveyForm/repository/studentSurveyRepository.java
@@ -1,3 +1,7 @@
+/*
+This interface helps the repository for managing student survey form data by extending JpaRepository to provide CRUD operations for the surveyData table.
+*/
+
 package _5.hw.assignment3.surveyForm.repository;
 
 import _5.hw.assignment3.surveyForm.model.studentSurveyData;
