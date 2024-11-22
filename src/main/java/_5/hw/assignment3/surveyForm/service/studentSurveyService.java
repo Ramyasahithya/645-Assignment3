9cba527900d4a88c0125d8c03471c6e96a1fdc9c
arsitha20 <158494218+arsitha20@users.noreply.github.com>
2024-11-22 13:00:49 -0500

    added comments

--- src/main/java/_5/hw/assignment3/surveyForm/service/studentSurveyService.java
+++ src/main/java/_5/hw/assignment3/surveyForm/service/studentSurveyService.java
@@ -1,3 +1,12 @@
+/*
+  Ramyasahithya Magani - G01425752
+  Arsitha Sathu - G01445215
+  Athiksha Venkannagari - G01461169
+  Sreshta Kosaraju - G01460468
+*/
+
+/* studentSurveyService interface define the service for managing student survey data, including CRUD operations such as Create, Update, Get and Delete.*/
+
 package _5.hw.assignment3.surveyForm.service;
 
 import _5.hw.assignment3.surveyForm.model.studentSurveyData;
