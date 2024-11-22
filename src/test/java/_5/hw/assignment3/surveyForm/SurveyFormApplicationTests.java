2112b7660016e55e0973eec30f46684c9e7b05da
arsitha20 <158494218+arsitha20@users.noreply.github.com>
2024-11-22 12:58:06 -0500

    added comments

--- src/test/java/_5/hw/assignment3/surveyForm/SurveyFormApplicationTests.java
+++ src/test/java/_5/hw/assignment3/surveyForm/SurveyFormApplicationTests.java
@@ -1,3 +1,12 @@
+/*
+  Ramyasahithya Magani - G01425752
+  Arsitha Sathu - G01445215
+  Athiksha Venkannagari - G01461169
+  Sreshta Kosaraju - G01460468
+*/
+
+// SurveyFormApplicationTest program contains test cases to verify if the Survey form application loads successfully.
+
 package _5.hw.assignment3.surveyForm;
 
 import org.junit.jupiter.api.Test;
